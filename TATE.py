f38a7767e10cea11a2a96c9a542ca494ecb0cbfb
Lazali51 <111712579+Lazali51@users.noreply.github.com>
2022-08-21 20:35:21 -0500

    Add files via upload

---
+++ TATE.py
@@ -0,0 +1,11 @@
+x = input("Do you like Tate, the TOP G\n" "1-Yes\n2-No\n3-Who's that\n> ")
+
+if x == ("1"):
+
+    print("You must be in HUSTLERS UNIVERSITY for sure")
+
+elif x == ("2"):
+    print("THAT'S WHY YOU DON'T HAVE 17 BUGATTI'S")
+
+elif x == ("3"):
+    print("YOU DON'T KNOW OF THE MASTER G WITH 17 BUGATTI'S AND 70W 10 L FIGHT RATIO")
